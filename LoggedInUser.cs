cf1b0864a3bb6b90f2c755e19ce7009b977d2409
Fredrik Lundvall <lundvall_fredrik@hotmail.com>
2020-03-22 12:55:33 +0100

    First commit

---
+++ LoggedInUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlowtorchesAndGunpowder
+{
+    public class LoggedInUser
+    {
+        public string Username;
+        public string Playername;
+        public string CurrentGamename;
+        public LoggedInUser(string aUsername, string aPlayername, string aCurrentGamename)
+        {
+            Username = aUsername;
+            Playername = aPlayername;
+            CurrentGamename = aCurrentGamename;
+        }
+        public override string ToString()
+        {
+            if(CurrentGamename == "")
+                return $"{Playername} ({Username})";
+            else
+                return $"{Playername} ({Username}) - Spelar just nu {CurrentGamename}";
+        }
+    }
+}
---
+++ LoginAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BlowtorchesAndGunpowder
+{
+    public class LoginAccount
+    {
+        public readonly string Username;
+        public readonly string Password;
+        public readonly string Playername;
+        public LoginAccount(string aUsername, string aPassword, string aPlayername)
+        {
+            Username = aUsername;
+            Password = aPassword;
+            Playername = aPlayername;
+        }
+    }
+}
